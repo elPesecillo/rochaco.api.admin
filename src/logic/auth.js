d3c4026156aa1dc9edb5aba87efa10574d6124f7
Arturo Galvan <agalvan0896@gmail.com>
2021-03-18 10:24:50 -0600

    commit

--- src/logic/auth.js
+++ src/logic/auth.js
@@ -21,6 +21,7 @@ const openApi = [
   "/api/suburb/saveStreet", //remover esta api de la lista
   "/api/suburb/getAllStreets", //remover este endpoint de la lista
   "/api/deleteUserInfo", //remover este endpoint de la lista
+  "/api/auth/appletoken", //remover este endpoint de la lista
 ];
 
 const protectedApi = ["/api/suburb/approveReject"];
